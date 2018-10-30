c89e49f32ce55c4da4f6a7fbdbf3dfcd9ccfabe8
pedrofr25 <44199740+pedrofr25@users.noreply.github.com>
2018-10-30 11:07:16 +0000

    Add files via upload

---
+++ FloatInput.py
@@ -0,0 +1,9 @@
+import math
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter first number: "))
+
+print("Quotient using // is", "\t", num1//num2)
+print("Quotient using / is", "\t", num1/num2)
+print("Remainder using % is", "\t", num1%num2)
+print("Exponent is ", "\t\t", num1**num2)
