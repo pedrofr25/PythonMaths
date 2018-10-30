5045899590b14f5a6cf0bea7dc2b62c2383ec265
pedrofr25 <44199740+pedrofr25@users.noreply.github.com>
2018-10-30 10:59:43 +0000

    Delete QuotientRemainder.py

--- QuotientRemainder.py
+++
@@ -1,8 +0,0 @@
-import math
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-print("Quotient using // is","\t", num1//num2)
-print("Quotient using / is","\t", num1/num2)
-print("Remainder using % is", "\t", num1%num2)
